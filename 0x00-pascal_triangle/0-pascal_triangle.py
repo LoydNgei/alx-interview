364dd24001e9c338043bea2a413a3025c7fb50c8
loyd ngei <loydngei@gmail.com>
2023-06-30 00:01:54 +0300

    README

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""a function to create pascal triangle"""
+""" A function to create pascal triangle """
 
 
 def pascal_triangle(n):
-
-    # Make sure it is a positive int
     if n <= 0:
         return []
 
-    # Initialize a variable pascal as a list of lists
     pascal = [[1]]
 
-    # Loop generates the remaining rows after row '[1]'
     for i in range(1, n):
         last_row = pascal[i - 1]
         this_row = [1]
