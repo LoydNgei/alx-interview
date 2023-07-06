db2c17c8d8ac39ea9066e263d2c4bb82d5c05343
loyd ngei <loydngei@gmail.com>
2023-07-07 00:19:06 +0300

    Lockboxs

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Write a method that determines if all the boxes can be opened """
+
+def canUnlockAll(boxes):
+    if len(boxes) == 0:
+        return True
+
+    keys = set(boxes[0])  # Keys obtained from the first box
+    checked = set([0])  # Set to track the visited boxes
+
+    # Perform BFS
+    while keys:
+        new_keys = set()
+
+        # Iterate through the keys obtained so far
+        for key in keys:
+            # Check if the key unlocks a new box and if that box is not already visited
+            if key < len(boxes) and key not in checked:
+                checked.add(key)
+                new_keys.update(boxes[key])  # Add the keys in the newly opened box
+
+        keys = new_keys  # Update the keys obtained in this iteration
+
+    # Check if all boxes have been visited
+    return len(checked) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+Tackling an interview question Algorithm problem on Lockboxes
