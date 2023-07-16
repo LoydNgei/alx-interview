af96e869ac1041ff46a65e5982fcb7ac3e8b8f6d
loyd ngei <loydngei@gmail.com>
2023-07-16 10:50:00 +0300

    Min Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+write a method that calculates the fewest number
+ of operations needed to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """Return an int else 0"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+ We handle an Algorithm on Minimum Operations in Python
