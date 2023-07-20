2b7bcc8fcb827d12fa89f3df1e1ca1ae23af53c2
Loyd Ngei <loydngei@gmail.com>
2023-07-20 19:41:27 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,37 +3,34 @@
 from sys import stdin
 
 
-def print_status_codes(status_counts, total_file_size):
+def print_status_codes(code, file_size):
     """Prints the status code with its count.
-    Format:
-        <status>: <count>
     """
-    print("Total file size: {}".format(total_file_size))
-    for status, count in sorted(status_counts.items()):
-        print("{}: {}".format(status, count))
+    print("File size: {}".format(file_size))
+    for key, val in code.items():
+        print("{}: {}".format(key, val))
 
 
-BATCH_SIZE = 10
-status_counts = {}
-total_file_size = 0
-line_count = 0
+code = {}
+file_size = 0
+vals_total = 0
 
 try:
     for line in stdin:
-        parts = line.split()
-        file_size = int(parts[-1])
-        total_file_size += file_size
-        status = int(parts[-2])
+        line = line.split()
+        file_size += int(line[-1])
+        status = line[-2]
+        vals = list(code.values())
+        vals_total = sum(vals)
 
-        if status in [200, 301, 400, 401, 403, 404, 405, 500]:
-            status_counts[status] = status_counts.get(status, 0) + 1
+        if status not in code:
+            code[status] = 1
+        else:
+            code[status] += 1
 
-        line_count += 1
+        vals_total += 1
 
-        if line_count % BATCH_SIZE == 0:
-            print_status_codes(status_counts, total_file_size)
-            status_counts = {}
-            total_file_size = 0
-
+        if vals_total % 10 == 0:
+            print_status_codes(code, file_size)
 except KeyboardInterrupt:
-    print_status_codes(status_counts, total_file_size)
+    print_status_codes(code, file_size)
